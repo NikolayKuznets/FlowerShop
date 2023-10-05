531cef3fbf63233916dba15de2d942d6a3dbc1da
NikolayKuz <nikolaykuz@yahoo.com>
2023-10-05 09:00:15 +0300

    Initial Commit

---
+++ FlowerShop/FlowerShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerShopApp.swift
+//  FlowerShop
+//
+//  Created by Ник К on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
